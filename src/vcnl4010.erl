75d1e602f3d621fc5c22cb5e34c2984af7d7b760
Will Vining <wfvining@gmail.com>
2018-06-30 17:37:04 +0000

    Begining i2c interface for IR proximity sensor

---
+++ src/vcnl4010.erl
@@ -0,0 +1,56 @@
+-module(vcnl4010).
+
+-export([set_led_intensity/2, set_frequency/2, start/1]).
+
+-define(VCNL4010_ADDR,      16#13).
+
+-define(COMMAND,          16#80).
+-define(PRODUCTID,        16#81).
+-define(PROXRATE,         16#82).
+-define(IRLED,            16#83).
+-define(AMBIENTPARAMETER, 16#84). 
+-define(AMBIENTDATA,      16#85).
+-define(PROXIMITYDATA,    16#87).
+-define(INTCONTROL,       16#89).
+-define(PROXIMITYADJUST,  16#8a).
+-define(INTSTAT,          16#8e).
+-define(MODTIMING,        16#8f).
+
+-type i2c_server_ref() :: pid().
+
+-spec start(string()) -> {ok, i2c_server_ref()}.
+start(Bus) ->
+    {ok, Device} = i2c:start_link(Bus, ?VCNL4010_ADDR),
+    ok = set_led_intensity(Device, 20),
+    ok = set_frequency(Device, 62.5),
+    ok = i2c:write(Device, <<?INTCONTROL:8,16#08>>),
+    {ok, Device }.
+
+
+%% Translate a frequency in samples per second to the corresponding
+%% code used by the device.
+-spec frequency(float()) -> integer().
+frequency(1.95)    -> 0;
+frequency(3.90625) -> 1;
+frequency(7.8125)  -> 2;
+frequency(16.625)  -> 3;
+frequency(31.25)   -> 4;
+frequency(62.5)    -> 5;
+frequency(125.0)   -> 6;
+frequency(250.0)   -> 7;
+frequency(_)       -> erlang:error(badarg).
+
+%% Set the sample frequency
+-spec set_frequency(i2c_server_ref(), float()) -> ok.
+set_frequency(Device, Freq) ->
+    F = frequency(Freq),
+    i2c:write(Device, <<?MODTIMING:8,F:8>>).
+
+%% Set the LED intensity, maximum of 20, any values larger than 20
+%% will be clipped.
+-spec set_led_intensity(i2c_server_ref(), integer()) -> ok.
+set_led_intensity(Device, I) when I > 20 ->
+    set_led_intensity(Device, 20);
+set_led_intensity(Device, I) ->
+    i2c:write(Device, <<?IRLED:8,I:8>>).
+
